75010d460374451f83165e537f5955286f4afbb9
Dikshita Patel <dikshipatel1999@gmail.com>
2024-06-13 11:55:54 -0400

    Initial Commit

---
+++ Fetch_TheMeal Dessert/Fetch_TheMeal_DessertApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fetch_TheMeal_DessertApp.swift
+//  Fetch_TheMeal Dessert
+//
+//  Created by Dikshita Rajendra Patel on 13/06/24.
+//
+
+import SwiftUI
+
+@main
+struct Fetch_TheMeal_DessertApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
